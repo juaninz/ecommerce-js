e61d96a5d180911bce11d6c1faf0b0e2745e8abc
Juan Nunez <juaninz2014@gmail.com>
2022-01-10 21:14:11 -0300

    proyecto js

---
+++ js/lista.js
@@ -0,0 +1,158 @@
+import {
+    productos
+} from "./productos.js";
+
+
+class ItemCarrito {
+    constructor(cantidad, item) {
+        this.cantidad = cantidad
+        this.item = item
+    }
+}
+
+let total = 0
+let carrito = JSON.parse(localStorage.getItem('carrito')) || []
+
+for (const producto of productos) {
+    let count = 1
+    $('#items').append(
+        `
+        <div class="card-body">
+            <h5 class="card-title">${ producto.nombre}</h5>
+            <img class="img-fluid" src="${producto.imagen}">
+            <p class="card-text">$ ${producto.precio}<p>
+            <div class="card-title agregar-producto">
+
+            Cantidad:
+            <select id="select-count-${producto.id}">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+            </select>
+            <button id="btn-add-${producto.id}" class="btn btn-primary addProduct"> Agregar </button>
+            </div>
+        </div>
+        `
+    )
+
+
+
+    //Acceder al select de cantidad para cada producto
+    $(`#select-count-${producto.id}`).change((event) => {
+        count = +event.target.value
+    })
+
+    //acceder evento agregar producto
+    $(`#btn-add-${producto.id}`).on('click', () => {
+        const itemCarrito = new ItemCarrito(count, producto)
+        addItemCarrito(itemCarrito)
+    })
+}
+
+const addItemCarrito = (item) => {
+    const itemCarrito = carrito.find(el => el.item.id === item['item']['id'])
+    if (!itemCarrito) {
+        carrito.push(item)
+        console.log(carrito)
+
+    } else {
+        itemCarrito['cantidad'] = item.cantidad
+        console.log(carrito)
+    }
+
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+
+    renderCarrito()
+
+}
+
+const renderCarrito = () => {
+
+    $('#generable-carrito').empty()
+    $('#generable-carrito').append(
+        `
+            <h2> Carrito </h2>
+        `
+    )
+    for (let el of carrito) {
+
+        $('#generable-carrito').append(
+            `
+                <li class='list-group-item text-right mx-2'> ${el.item.nombre} - Cantidad: ${el.cantidad} <button id="quitar-${el.item.id}"  class="btn btn-danger"> Quitar </button></li>
+                
+            `
+        )
+        $(`#quitar-${el.item.id}`).on('click', () => {
+            carrito = carrito.filter((itemCarrito) => {
+                let carritoSinItem = itemCarrito.item.id !== el.item.id;
+                return carritoSinItem
+            })
+            localStorage.setItem('carrito', JSON.stringify(carrito))
+            renderCarrito()
+        })
+    }
+    //Le agrego para que se actualice el total también
+    calcularTotal();
+}
+
+renderCarrito()
+
+//CALCULAR TOTAL DE MIS PRODUCTOS
+function calcularTotal() {
+    // Limpiamos precio anterior
+    total = 0;
+    // Recorremos el array del carrito
+    carrito.forEach((item) => {
+        // De cada elemento obtenemos su precio
+        console.log(item);
+        total = total + item.item.precio * item.cantidad; // Sumo el precio al total
+
+    });
+    // Renderizamos el precio en el HTML
+    // Hago el append directamente en la sección carrito
+    $('#generable-carrito').append(
+        `   
+            <p class="total">Total:$ <span id="total">${total.toFixed(2)}</span></p>
+            
+        `
+    ) // Uso directamente lo que quiero que se agregue (Lo saqué del html)
+}
+
+//agrego funcion vaciar carrito y el evento del boton vaciar
+
+function vaciarCarrito() {
+    // Limpiamos los Productos guardados
+    carrito = [];
+    localStorage.clear();
+    // Renderizamos los cambios
+    renderCarrito();
+}
+
+
+$(`#boton-vaciar`).on('click', () => {
+    vaciarCarrito()
+    $("#generable-carrito").append('<p id="mensaje">Carrito vaciado!</p>');
+    $("#mensaje").css("color", "red")
+        .slideDown(5000)
+        .slideUp(3000);
+    $(".total").hide()
+})
+
+$(`#boton-comprar`).on('click', () => {
+    vaciarCarrito()
+    $("#generable-carrito").append('<p id="mensaje">Compra Realizada!</p>');
+    $("#mensaje").css("color", "green")
+        .slideDown(5000)
+        .slideUp(3000);
+    $(".total").hide()
+})
+
+$("#generable-carrito").hide();
+$("#boton-vaciar").hide();
+$("#boton-comprar").hide();
+$(".addProduct").click(() => {
+    $("#generable-carrito").slideDown("fast");
+    $("#boton-vaciar").slideDown("fast");
+    $("#boton-comprar").slideDown("fast");
+});
+
---
+++ js/productos.js
@@ -0,0 +1,114 @@
+const productos = [{
+    id: 1,
+    nombre: 'Camiseta de Boca',
+    precio: 8299.59,
+    imagen: './images/boca.jpg'
+},
+{
+    id: 2,
+    nombre: 'Camiseta de Boca Suplente',
+    precio: 8100.10,
+    imagen: 'images/boca_suplente.jpg'
+},
+{
+    id: 3,
+    nombre: 'Camiseta de River',
+    precio: 8250.14,
+    imagen: 'images/river_titular.jpg'
+},
+{
+    id: 4,
+    nombre: 'Camiseta de River Suplente',
+    precio: 4650.5,
+    imagen: 'images/river_suplente.jpg'
+},
+{
+    id: 5,
+    nombre: 'Camiseta de Independiente',
+    precio: 5000.58,
+    imagen: 'images/independiente.jpg'
+},
+{
+    id: 6,
+    nombre: 'Camiseta de Racing',
+    precio: 5000.60,
+    imagen: 'images/racing.jpg'
+},
+{
+    id: 7,
+    nombre: 'Camiseta de Central',
+    precio: 5000.58,
+    imagen: 'images/central.jpg'
+},
+{
+    id: 8,
+    nombre: 'Camiseta de Newells',
+    precio: 5000.58,
+    imagen: 'images/newells.jpg'
+},
+{
+    id: 9,
+    nombre: 'Camiseta de Velez',
+    precio: 5000.58,
+    imagen: 'images/velez.jpg'
+},
+{
+    id: 10,
+    nombre: 'Camiseta de Estudiantes',
+    precio: 5000.58,
+    imagen: 'images/estudiantes.jpg'
+},
+{
+    id: 11,
+    nombre: 'Camiseta de San Lorenzo',
+    precio: 5000.58,
+    imagen: 'images/sanlorenzo.jpg'
+},
+{
+    id: 12,
+    nombre: 'Camiseta de Colon',
+    precio: 5000.58,
+    imagen: 'images/colon.jpg'
+},
+{
+    id: 13,
+    nombre: 'Camiseta de Real Madrid',
+    precio: 5000.58,
+    imagen: 'images/realmadrid.jpg'
+},
+{
+    id: 14,
+    nombre: 'Camiseta de Barcelona',
+    precio: 5000.58,
+    imagen: 'images/barcelona.jpg'
+},
+{
+    id: 15,
+    nombre: 'Camiseta de PSG',
+    precio: 5000.58,
+    imagen: 'images/psg.jpg'
+},
+{
+    id: 16,
+    nombre: 'Pelota Telstar',
+    precio: 4000.55,
+    imagen: 'images/pelota_telstar.jpg'
+},
+{
+    id: 17,
+    nombre: 'Guantes',
+    precio: 4500.89,
+    imagen: 'images/guantes.jpg'
+},
+{
+    id: 18,
+    nombre: 'Canilleras',
+    precio: 5000.58,
+    imagen: 'images/canilleras.jpg'
+},
+];
+
+
+export {
+    productos
+}
